e2c805ae365383ae0448782fa1d29e112ee3057f
yurifeng <yuri_feng@outlook.com>
2018-09-10 15:14:07 +0800

    添加View视图

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult UseExtension()
+        {
+            //创建ShoppingCart实例
+            ShoppingCart cart = new ShoppingCart()
+            {
+                Products = new List<Product>()
+                {
+                    new Product(){Name = "yty",Price = 45},
+                    new Product(){Name = "livir",Price = 45},
+                    new Product(){Name = "owen",Price = 45},
+                    new Product(){Name = "van",Price = 45},
+                }
+            };
+            //ShoppingCart用扩展的TotalPrices方法,得到了总价格
+            decimal totalPrices = cart.TotalPrices();
+
+            //返回View视图
+            return View("Suprise", (object)string.Format("Total:{0:C}", totalPrices));
+        }
+    }
+}
